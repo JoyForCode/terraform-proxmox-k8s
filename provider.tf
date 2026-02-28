c940e21dc27de75609259470ea91ab7629cd4b60
JoyForCode <jigneshparatane@gmail.com>
2026-02-28 17:37:59 +0530

    Added the ssh block and populated the expected fields by refering to the provider's documentation.

--- provider.tf
+++ provider.tf
@@ -14,4 +14,9 @@ provider "proxmox" {
 	endpoint = "https://192.168.1.10:8006/api2/json"
 	insecure = true
 	api_token = data.vault_kv_secret_v2.proxmox.data["api_token"]
+
+	ssh {
+		agent = false
+		private_key = data.vault_kv_secret_v2.proxmox.data["ssh_private_key"]
+	}
 }
