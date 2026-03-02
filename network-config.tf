a6ce8056ad5f107887e941d7e719abf06c37aa6c
JoyForCode <jigneshparatane@gmail.com>
2026-03-02 08:25:15 +0530

    Changed the fixed ens18 ethernet interface to match the required interface as the VM is a q35 not a i1440fx type, and also changed the file name in output to correctly escape the characters.

--- network-config.tf
+++ network-config.tf
@@ -7,7 +7,9 @@ resource "proxmox_virtual_environment_file" "network_config" {
     data = <<-EOF
 version: 2
 ethernets:
-  ens18:
+  id0:
+    match:
+      driver: virtio_net
     addresses: 
       - ${data.vault_kv_secret_v2.k8s_base.data["ipv4_address"]}
     gateway4: ${data.vault_kv_secret_v2.k8s_base.data["gateway"]}
@@ -16,6 +18,6 @@ ethernets:
         - ${data.vault_kv_secret_v2.k8s_base.data["dns"]}
 EOF
 
-    file_name = "network-config.yaml"
+    file_name = "network_config.yaml"
   }
 }
