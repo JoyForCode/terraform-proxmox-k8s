48e92b563a177e26b7ae094564c5420be19d6957
JoyForCode <jigneshparatane@gmail.com>
2026-03-02 14:04:47 +0530

    Changed the data keyword to ephemeral to have better secret handling and avoid leaking of secrets.

--- vault_secrets.tf
+++ vault_secrets.tf
@@ -1,9 +1,9 @@
-data "vault_kv_secret_v2" "k8s_base" {
+ephemeral "vault_kv_secret_v2" "k8s_base" {
   mount = "terraform"
   name  = var.vault_k8s_base_path
 }
 
-data "vault_kv_secret_v2" "proxmox" {
+ephemeral "vault_kv_secret_v2" "proxmox" {
   mount = "terraform"
   name  = var.vault_proxmox_path
 }
