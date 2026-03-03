337ae852354dc5d58502138a42601cc93e0b9595
JoyForCode <jigneshparatane@gmail.com>
2026-03-03 17:47:54 +0530

    Added two variables in variables.tf to be used for multi VM deployment in proxmox.

--- variables.tf
+++ variables.tf
@@ -19,6 +19,18 @@ variable "vault_k8s_base_path" {
   default = "k8s-base"
 }
 
+variable "vault_k8s_nodes_path" {
+  type = string
+  description = "Vault base path for k8s node configs"
+  default = "k8s-nodes"
+}
+
+variable "k8s_node_names" {
+  type = list(string)
+  description = "List of k8s node names matching Vault secret paths"
+  default = ["k8s-control-plane", "k8s-worker-1", "k8s-worker-2"]
+}
+
 variable "vault_proxmox_path" {
   type    = string
   default = "proxmox"
